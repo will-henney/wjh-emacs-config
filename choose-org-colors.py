bf04a13151fdc33968457c22420a29e6d5e6c4f2
William Henney <will@henney.org>
2012-09-09 13:44:09 -0500

    Modeline font customization for emacs-powerline
    * wjh-emacs-config.org: This is where we actually made the changes
    * wjh-misc-appearance-theme.el: Add modeline font customization

--- choose-org-colors.py
+++ choose-org-colors.py
@@ -1,28 +1,28 @@
 
-"""
-Interpolate eight colors between two limits
-"""
-from grapefruit import Color
-import numpy as np
-import sys
-try: 
-    spec1, spec2 = sys.argv[1:3]
-    color1 = Color.NewFromHtml(spec1)
-    color2 = Color.NewFromHtml(spec2)
-except: 
-    print """
-Usage: %s spec1 spec2
-
-Where spec1 and spec2 are valid HTML color specs
-""" % (sys.argv[0])
-    sys.exit()
-
-NCOLORS = 8
-fracs = np.linspace(1.0, 0.0, NCOLORS)
-colors = [color1.Blend(color2, frac) for frac in fracs]
-
-facespec="'(org-level-%i ((((class color) (background dark)) (:foreground \"%s\"))))"
-print ";; %s to %s" % (spec1, spec2)
-for i, c in enumerate(colors):
-    print facespec % (i+1, c.html)
-print
+  """
+  Interpolate eight colors between two limits
+  """
+  from grapefruit import Color
+  import numpy as np
+  import sys
+  try: 
+      spec1, spec2 = sys.argv[1:3]
+      color1 = Color.NewFromHtml(spec1)
+      color2 = Color.NewFromHtml(spec2)
+  except: 
+      print """
+  Usage: %s spec1 spec2
+  
+  Where spec1 and spec2 are valid HTML color specs
+  """ % (sys.argv[0])
+  
+  NCOLORS = 8
+  fracs = np.linspace(1.0, 0.0, NCOLORS)
+  colors = [color1.Blend(color2, frac) for frac in fracs]
+  
+  facespec="'(org-level-%i ((((class color) (background dark)) (:foreground \"%s\"))))"
+  print ";; %s to %s" % (spec1, spec2)
+  for i, c in enumerate(colors):
+      print facespec % (i+1, c.html)
+  print
+  
