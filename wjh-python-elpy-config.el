b5ec34385819441b41e3a40d22bd033b1754329e
William Henney <will@henney.org>
2013-11-08 13:42:09 -0600

    Resolve key conflicy between elpy and smartscan

--- wjh-python-elpy-config.el
+++ wjh-python-elpy-config.el
@@ -2,3 +2,9 @@
 (setq python-check-command "pyflakes")
 (elpy-use-ipython)
 (elpy-clean-modeline)
+;; WJH 08 Nov 2013 - we prefer to use these for smartscan
+(define-key elpy-mode-map (kbd "M-n") nil)
+(define-key elpy-mode-map (kbd "M-p") nil)
+;; Use the fn key instead
+(define-key elpy-mode-map (kbd "H-n") 'elpy-nav-forward-definition)
+(define-key elpy-mode-map (kbd "H-p") 'elpy-nav-backward-definition)
