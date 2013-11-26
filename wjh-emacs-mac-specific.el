a73600216e1a206c43d6c7454061ba11ec08bd5b
William Henney <will@henney.org>
2013-11-26 09:40:07 -0600

    Fix my PATH in eshell - hurray!

--- wjh-emacs-mac-specific.el
+++ wjh-emacs-mac-specific.el
@@ -88,3 +88,27 @@
   nil)
 (global-set-key [wheel-left] 'wjh/drop-event)
 (global-set-key [wheel-right] 'wjh/drop-event)
+
+;; Sort out my PATH variables at last
+;; Copied from this gist:
+;; https://gist.github.com/bradleywright/2046593
+
+;; This sets the Emacs "PATH" environment variable and the `exec-path` 
+;; variable to the same value your login shell sees. The reason this 
+;; is necessary is because of this:
+;; 
+;; http://developer.apple.com/library/mac/#qa/qa1067/_index.html
+;;
+;; Basically apps launched from Finder inherit their environment from
+;; a .plist file rather than the shell environment.
+(defun set-exec-path-from-shell-PATH ()
+  "Sets the exec-path to the same value used by the user shell"
+  (let ((path-from-shell
+         (replace-regexp-in-string
+          "[[:space:]\n]*$" ""
+          (shell-command-to-string "$SHELL -l -c 'echo $PATH'"))))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+ 
+;; call function now
+(set-exec-path-from-shell-PATH)
