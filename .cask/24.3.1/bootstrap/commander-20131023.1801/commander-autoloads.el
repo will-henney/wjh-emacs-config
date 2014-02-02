ec46240adac2097e152e723360190a7d5f817538
William Henney <will@henney.org>
2014-02-01 18:01:09 -0600

    Cleanup from cask upgrade

--- .cask/24.3.1/bootstrap/commander-20131023.1801/commander-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; commander-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("commander-pkg.el" "commander.el") (21130
-;;;;;;  61389 520156 0))
-
-;;;***
-
-(provide 'commander-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; commander-autoloads.el ends here
