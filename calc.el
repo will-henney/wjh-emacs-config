ac3d2f0e483b65768d78621654ba6a53b43935cb
William Henney <will@henney.org>
2014-07-04 18:13:49 -0500

    Add units to calc config
    Can't get it to work properly in org babel

---
+++ calc.el
@@ -0,0 +1,15 @@
+
+;;; Custom units stored by Calc on Fri Jul  4 16:49:10 2014
+(setq math-additional-units 
+      '((Msun "1.989e33 * g" "Solar Mass")
+	(Mjup "1.8986e30 * g" "Jupiter mass")
+	(Mearth "5.976e27 * g" "Earth Mass")
+	(lyr "9.460536207e17 * cm" "Lightyear")
+	(pc "3.085677582e18 * cm" "Parsec")
+	(Rsun "6.96e10 * cm" "Solar radius")
+	(Rearth "6.378e8 * cm" "Earth radius")
+	(Lsun "3.82e33 * erg/s"  "Solar Luminosity")
+	(AU "1.49597870691d13 * cm" "Astronomical unit")
+	(Jansky "1d-23 erg/cm^2 s Hz" "Jansky"))
+)
+;;; End of custom units
