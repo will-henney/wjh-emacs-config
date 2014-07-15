eb3c2279528c9c119e37c9350bd91a1496cab76e
William Henney <will@henney.org>
2014-07-15 11:08:36 -0500

    Tweaks to helm
    - Better highlighting of current selection
    - Truncate lines in selection buffer
    - Try out hem-adapt

--- wjh-helm-config.el
+++ wjh-helm-config.el
@@ -5,6 +5,7 @@
 (require 'helm-eshell)
 (require 'helm-files)
 (require 'helm-grep)
+(require 'helm-adaptive)
 
 ; rebind tab to do persistent action
 (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) 
@@ -19,6 +20,7 @@
 
 (setq
  helm-google-suggest-use-curl-p t
+ helm-truncate-lines t
  helm-scroll-amount 4 ; scroll 4 lines other window using
 		      ; M-<next>/M-<prior>
  helm-quick-update t ; do not display invisible candidates
@@ -39,7 +41,7 @@
  helm-M-x-requires-pattern 0     ; show all candidates when set to 0
  helm-boring-file-regexp-list
  '("\\.git$" "\\.hg$" "\\.svn$" "\\.CVS$"
-   "\\._darcs$" "\\.la$" "\\.o$" "\\.i$") ; do not show these files in helm buffer
+   "\\._darcs$" "\\.la$" "\\.o$" "\\.i$" "\\~$") ; do not show these files in helm buffer
  helm-ff-file-name-history-use-recentf t
  helm-move-to-line-cycle-in-source t ; move to end or beginning of source
                                         ; when reaching top or bottom of source.
@@ -91,3 +93,4 @@
 (global-set-key (kbd "C-c h d") 'helm-dash)
 
 (helm-mode 1)
+(helm-adaptive-mode 1)
