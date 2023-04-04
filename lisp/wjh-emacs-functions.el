6487575cf20c2dce20e03c6532104097603422d7
William Henney <will@henney.org>
2023-04-04 12:34:51 -0600

    Add a duplicate-line function bound to Cmd-d

--- lisp/wjh-emacs-functions.el
+++ lisp/wjh-emacs-functions.el
@@ -232,3 +232,43 @@ recent first."
 	(delete-region (car bounds) (cdr bounds))
 	(insert newtext)))))
 (global-set-key (kbd "C-c w") 'wjh/convert-float-to-clean-form)
+
+
+;; 2023-04-04: Duplicate line function to mimic behavior of Cmd-D in
+;; JetBrains editors. This is copied from
+;; https://stackoverflow.com/a/998472/353062
+(defun duplicate-line (arg)
+  "Duplicate current line, leaving point in lower line."
+  (interactive "*p")
+
+  ;; save the point for undo
+  (setq buffer-undo-list (cons (point) buffer-undo-list))
+
+  ;; local variables for start and end of line
+  (let ((bol (save-excursion (beginning-of-line) (point)))
+        eol)
+    (save-excursion
+
+      ;; don't use forward-line for this, because you would have
+      ;; to check whether you are at the end of the buffer
+      (end-of-line)
+      (setq eol (point))
+
+      ;; store the line and disable the recording of undo information
+      (let ((line (buffer-substring bol eol))
+            (buffer-undo-list t)
+            (count arg))
+        ;; insert the line arg times
+        (while (> count 0)
+          (newline)         ;; because there is no newline in 'line'
+          (insert line)
+          (setq count (1- count)))
+        )
+
+      ;; create the undo information
+      (setq buffer-undo-list (cons (cons eol (point)) buffer-undo-list)))
+    ) ; end-of-let
+
+  ;; put the point in the lowest line and return
+  (next-line arg))
+(global-set-key (kbd "s-d") 'duplicate-line)
